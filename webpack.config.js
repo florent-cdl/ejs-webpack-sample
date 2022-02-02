635fc382e04bfccc3792d0d08190bdc0ecf3e540
florent-cdl <fj@carredelune.com>
2022-02-02 11:17:47 +0100

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { htmlWebpackPluginTemplateCustomizer }  = require('template-ejs-loader')
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname) + '/src/index.js',
+  module: {
+    rules: [
+      // { test: /\.ejs$/i, use: [ { loader: 'ejs-easy-loader' } ] }
+      { test: /\.ejs$/i, use: [ 'html-loader', 'template-ejs-loader' ] }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      filename: './page-a.html',
+      template: htmlWebpackPluginTemplateCustomizer({
+        htmlLoaderOption:{
+          // set individual html-loader option here
+        },
+        templateEjsLoaderOption:{
+          root:'', // set individual template-ejs-loader option here
+          data: {
+            title: 'Page A Title',
+            meta: {'keywords': 'word1, word2', 'description': 'page description'},
+            name: "a",
+          }
+        },
+        template: './src/template.ejs',
+      }),
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      filename: './page-b.html',
+      title: 'Page B Title',
+      template: htmlWebpackPluginTemplateCustomizer({
+        htmlLoaderOption:{
+          // set individual html-loader option here
+        },
+        templateEjsLoaderOption:{
+          root:'', // set individual template-ejs-loader option here
+          data: {
+            name: "b",
+            obj: {"food": "fruit"},
+            arr: ["apple", "orange", "banana"],
+          }
+        },
+        template: './src/template.ejs',
+      }),
+    }),
+  ]
+};
